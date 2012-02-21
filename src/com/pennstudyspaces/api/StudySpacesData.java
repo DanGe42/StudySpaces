6f8b6c1417feea9b74e59f29252e6f123ff69ede
Daniel Ge <dange@seas.upenn.edu>
2012-02-21 03:11:19 -0500

    Added API related classes.

---
+++ src/com/pennstudyspaces/api/StudySpacesData.java
@@ -0,0 +1,12 @@
+package com.pennstudyspaces.api;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel Ge
+ * Date: 2/21/12
+ * Time: 1:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StudySpacesData {
+
+}
