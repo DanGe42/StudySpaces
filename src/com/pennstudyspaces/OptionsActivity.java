70d4b30e8377c00524115804faa6dbafae3a30bd
U-CoryRivera-PC\Aelorix <Aelorix@CoryRivera-PC.(none)>
2012-03-03 22:23:58 -0500

    Added checks for incorrect input in OptionsActivity

--- src/com/pennstudyspaces/OptionsActivity.java
+++ src/com/pennstudyspaces/OptionsActivity.java
@@ -30,11 +30,8 @@ public class OptionsActivity extends Activity {
 		String buffer = ((EditText)findViewById(R.id.num_people)).getText().toString();
 		
 		int numPeople = 1;
-		try {
+		if(!buffer.equals("")) {
 			numPeople = Integer.parseInt(buffer);
-		}
-		catch(Exception e) {
-			//Show a toast for invalid number entries?
 		}
 		
 		//Start & End Time fields
@@ -48,7 +45,20 @@ public class OptionsActivity extends Activity {
 		}
 		else {
 			fromTimeHour = Integer.parseInt(fromTime.substring(0, fromTime.indexOf(":")));
+			if(fromTimeHour < 0) {
+				fromTimeHour = 0;
+			}
+			else if(fromTimeHour > 23) {
+				fromTimeHour = 23;
+			}
+			
 			fromTimeMin = Integer.parseInt(fromTime.substring(fromTime.indexOf(":")+1,fromTime.length()));
+			if(fromTimeMin < 0) {
+				fromTimeMin = 0;
+			}
+			else if(fromTimeMin > 59) {
+				fromTimeMin = 59;		
+			}
 		}
 		
 		toTime = ((EditText)findViewById(R.id.to)).getText().toString();
@@ -59,7 +69,20 @@ public class OptionsActivity extends Activity {
 		}
 		else {
 			toTimeHour = Integer.parseInt(toTime.substring(0, toTime.indexOf(":")));
+			if(toTimeHour < 0) {
+				toTimeHour = 0;
+			}
+			else if(toTimeHour > 23) {
+				toTimeHour = 23;
+			}
+			
 			toTimeMin = Integer.parseInt(toTime.substring(toTime.indexOf(":")+1,toTime.length()));
+			if(toTimeMin < 0) {
+				toTimeMin = 0;
+			}
+			else if(toTimeMin > 59) {
+				toTimeMin = 59;
+			}
 		}
 		
 		//Date Field
@@ -76,8 +99,20 @@ public class OptionsActivity extends Activity {
 			int secondSlash = date.indexOf("/", slashIndex+1);
 			
 			month = Integer.parseInt(date.substring(0,slashIndex));
+			if(month > 12)
+				month = 12;
+			else if(month < 1)
+				month = 1;
+			
 			day = Integer.parseInt(date.substring(slashIndex+1,secondSlash));
+			if(day < 1)
+				day = 1;
+			else if(day > 31)
+				day = 31;
+			
 			year = Integer.parseInt(date.substring(secondSlash+1,date.length()));
+			if(year < 2011)
+				year = 2011;
 		}
 
 		
