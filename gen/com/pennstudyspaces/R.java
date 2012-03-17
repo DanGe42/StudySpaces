35d082d809fca1b44d0e14de7d994bacc131f0f6
U-CoryRivera-PC\Aelorix <Aelorix@CoryRivera-PC.(none)>
2012-03-16 22:11:48 -0400

    Changed some more UI elements and added a few more styles

--- gen/com/pennstudyspaces/R.java
+++ gen/com/pennstudyspaces/R.java
@@ -17,34 +17,34 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int back=0x7f060013;
-        public static final int computer_check=0x7f060010;
-        public static final int date=0x7f06000d;
-        public static final int from=0x7f06000a;
-        public static final int fromLabel=0x7f060009;
+        public static final int back=0x7f060008;
+        public static final int computer_check=0x7f060012;
+        public static final int date=0x7f06000f;
+        public static final int from=0x7f06000c;
+        public static final int fromLabel=0x7f06000b;
         public static final int item_amenities=0x7f060007;
         public static final int item_building_name=0x7f060004;
         public static final int item_num_rooms=0x7f060006;
         public static final int item_room_kind=0x7f060005;
         public static final int map_container=0x7f06001b;
-        public static final int num_people=0x7f060008;
-        public static final int private_check=0x7f06000e;
-        public static final int projector_check=0x7f060011;
-        public static final int refresh_button=0x7f060000;
+        public static final int num_people=0x7f06000a;
+        public static final int private_check=0x7f060010;
+        public static final int projector_check=0x7f060013;
+        public static final int refresh_button=0x7f060001;
         public static final int roomTitle=0x7f060014;
-        public static final int search_button=0x7f060001;
+        public static final int search_button=0x7f060000;
         public static final int spaces_list=0x7f060002;
         public static final int spaces_list_empty=0x7f060003;
-        public static final int submit=0x7f060012;
+        public static final int submit=0x7f060009;
         public static final int text_building=0x7f060015;
         public static final int text_computer=0x7f060018;
         public static final int text_occupancy=0x7f060016;
         public static final int text_privacy=0x7f06001a;
         public static final int text_projector=0x7f060017;
         public static final int text_whiteboard=0x7f060019;
-        public static final int to=0x7f06000c;
-        public static final int toLabel=0x7f06000b;
-        public static final int whiteboard_check=0x7f06000f;
+        public static final int to=0x7f06000e;
+        public static final int toLabel=0x7f06000d;
+        public static final int whiteboard_check=0x7f060011;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -78,6 +78,8 @@ public final class R {
         public static final int CheckboxText=0x7f050003;
         public static final int ColorTitle=0x7f050001;
         public static final int PlainText=0x7f050000;
+        public static final int RoomDetailText=0x7f050004;
+        public static final int RoomDetailValue=0x7f050005;
         public static final int TextField=0x7f050002;
     }
 }
