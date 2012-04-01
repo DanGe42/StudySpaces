24c3aab05b81f4f2c444b39ce5a02f06330ab0b6
Daniel Ge <dange@seas.upenn.edu>
2012-03-31 21:34:11 -0400

    Set up default search preferences for the Search dialog.
    Refactored SearchActivity

--- src/com/pennstudyspaces/SearchActivity.java
+++ src/com/pennstudyspaces/SearchActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -19,16 +20,20 @@ import android.widget.TextView;
 
 public class SearchActivity extends Activity {
 	private static final String TAG = SearchActivity.class.getSimpleName();
+
+    private StudySpacesApplication app;
 	
-	private boolean priv,wboard,computer,projector;
+	private boolean priv, wboard, computer, projector;
 	private int numPeople;
-	private int fromTimeHour,fromTimeMin;
+	private int fromTimeHour, fromTimeMin;
 	private int toTimeHour, toTimeMin;	
-	private int day,month,year;
+	private int day, month, year;
 	
 	private TextView dateDisplay;
 	private Button dateButton;
 	private DatePickerDialog.OnDateSetListener dateListener;
+    private CheckBox privCheck, wboardCheck, compCheck, projCheck;
+
 	public final static int DATE_DIALOG_ID = 0;
 	
 	private Spinner toSpinner;
@@ -38,61 +43,90 @@ public class SearchActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.options);
-        
+
+        app = (StudySpacesApplication) getApplication();
+
+        expandPrefs();
+        initDateToggles();
+
 		numPeople = 1;
-		priv = wboard = computer = projector = false;
+
 		fromTimeHour = 0;
-		fromTimeMin = 0;
+		fromTimeMin  = 0;
 		toTimeHour = 23;
-		toTimeMin = 0;
-		
-		Calendar c = Calendar.getInstance();
-		day = c.get(Calendar.DAY_OF_MONTH);
-		month = c.get(Calendar.MONTH);
-		year = c.get(Calendar.YEAR);
-	
-		dateButton = (Button) findViewById(R.id.dateButton);
-		dateDisplay = (TextView) findViewById(R.id.dateDisplay);
-		
-		//Update display updates currently displayed date
-		updateDisplay();
-		
-		dateButton.setOnClickListener(new View.OnClickListener() {		
-			@Override
-			public void onClick(View v) {
-				showDialog(DATE_DIALOG_ID);
-			}
-		});
-		
-		dateListener = new DatePickerDialog.OnDateSetListener() {
-			@Override
-			public void onDateSet(DatePicker view, int inputYear, int monthOfYear,
-				int dayOfMonth) {
-					year = inputYear;
-					month = monthOfYear;
-					day = dayOfMonth;
-					updateDisplay();
-			}
-		};
-		
+		toTimeMin  = 0;
+
 		//Set up the spinner adapters
-		toSpinner = (Spinner)findViewById(R.id.toTime);
-		fromSpinner = (Spinner)findViewById(R.id.fromTime);
-		
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-				this, R.array.time_array, 
-				android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(
-				android.R.layout.simple_spinner_dropdown_item);
-		
-		toSpinner.setAdapter(adapter);
-		fromSpinner.setAdapter(adapter);
-		
-		//Set default time to 9 - 10AM, seeing as no one really wants to
-		//reserve a room at midnight.
-		fromSpinner.setSelection(9);
-		toSpinner.setSelection(10);
+		initSpinners();
 	}
+
+    private void initDateToggles() {
+        Calendar c = Calendar.getInstance();
+        day = c.get(Calendar.DAY_OF_MONTH);
+        month = c.get(Calendar.MONTH);
+        year = c.get(Calendar.YEAR);
+
+        dateButton = (Button) findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        dateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int inputYear, int monthOfYear,
+                                  int dayOfMonth) {
+                year = inputYear;
+                month = monthOfYear;
+                day = dayOfMonth;
+                updateDisplay();
+            }
+        };
+
+        //Update display updates currently displayed date
+        dateDisplay = (TextView) findViewById(R.id.dateDisplay);
+        updateDisplay();
+    }
+
+    private void initSpinners() {
+        toSpinner   = (Spinner) findViewById(R.id.toTime);
+        fromSpinner = (Spinner) findViewById(R.id.fromTime);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.time_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        toSpinner.setAdapter(adapter);
+        fromSpinner.setAdapter(adapter);
+
+        //Set default time to 9 - 10AM, seeing as no one really wants to
+        //reserve a room at midnight.
+        fromSpinner.setSelection(9);
+        toSpinner.setSelection(10);
+    }
+
+    private void expandPrefs() {
+        SharedPreferences prefs = app.getPrefs();
+
+        privCheck   = (CheckBox) findViewById(R.id.private_check);
+        wboardCheck = (CheckBox) findViewById(R.id.whiteboard_check);
+        compCheck   = (CheckBox) findViewById(R.id.computer_check);
+        projCheck   = (CheckBox) findViewById(R.id.projector_check);
+
+        priv      = prefs.getBoolean("private",    false);
+        wboard    = prefs.getBoolean("whiteboard", false);
+        computer  = prefs.getBoolean("computer",   false);
+        projector = prefs.getBoolean("projector",  false);
+
+        privCheck.setChecked(priv);
+        wboardCheck.setChecked(wboard);
+        compCheck.setChecked(computer);
+        projCheck.setChecked(projector);
+    }
 	
 	public void submitOptions(View view) {
 		//Get values from various fields
@@ -120,10 +154,10 @@ public class SearchActivity extends Activity {
 		toTimeHour = toPos;
 		
 		//Amenities Fields
-		priv = ((CheckBox)findViewById(R.id.private_check)).isChecked();
-		wboard = ((CheckBox)findViewById(R.id.whiteboard_check)).isChecked();
-		projector = ((CheckBox)findViewById(R.id.projector_check)).isChecked();
-		computer = ((CheckBox)findViewById(R.id.computer_check)).isChecked();
+		priv      = privCheck.isChecked();
+		wboard    = wboardCheck.isChecked();
+		projector = projCheck.isChecked();
+		computer  = compCheck.isChecked();
 		
         int[] intArray = {numPeople,fromTimeHour,fromTimeMin,toTimeHour,toTimeMin,month,day,year};
         boolean[] boolArray = {priv,wboard,projector,computer};
